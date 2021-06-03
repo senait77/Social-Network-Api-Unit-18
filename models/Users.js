6fe533b562f6bbb3ca8f62d8f22b2bad688f78f1
Senait <senutekie77@gmail.com>
2021-06-03 00:59:04 -0700

    users shcema done

---
+++ models/Users.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UsersSchema = new Schema(
+    {
+        username: {
+           type: String,
+           unique: true,
+           required: true,
+           trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // use REGEX to validate correct email
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+            
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+
+        },
+        id: false
+    }
+)
+
+//get total count of friends
+UsersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+// create the users model using the users schema
+const Users = model('Users', UsersSchema);
+
+module.exports = Users;
