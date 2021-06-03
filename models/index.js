85486a00860e52112173c88ef23fac143e98c3c3
Senait <senutekie77@gmail.com>
2021-06-03 01:43:59 -0700

    thoughts and reaction schemas set up

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// require Thoughts snd Users model
+const Thoughts = require('./Thoughts');
+const Users = require('./Users');
+
+
+module.exports = { Users, Thoughts }; 
+
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,5 @@
+// require express router
+const router = require('express').Router();
+
+// 
+
