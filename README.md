2e5ec59a39e6cda45f19f64379a2a54a217aef19
Senait <senutekie77@gmail.com>
2021-06-03 02:30:40 -0700

    readme setup

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# Social-Network-Api-Unit-18
+
+<h1 align="center"> Social Networking API 👋</h1>
+  
+<p align="center">
+    <img src="https://img.shields.io/badge/javascript-yellow" />
+    <img src="https://img.shields.io/badge/express-orange" />
+    <img src="https://img.shields.io/badge/MongoDB-blue"  />
+    <img src="https://img.shields.io/badge/mongoose-red"  />
+    <img src="https://img.shields.io/badge/moment-blue"  />
+    <img src="https://img.shields.io/badge/nodemon-green" />
+</p>
+   
+## Description
+
+🔍 an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.
+  
+💻 Below is a video of the application:
+  
+![social-network-api](./assets/users.gif)
+
+## User Story
+
+```
+AS A social media startup
+I WANT an API for my social network that uses a NoSQL database
+SO THAT my website can handle large amounts of unstructured data
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a social network API
+WHEN I enter the command to invoke the application
+THEN my server is started and the Mongoose models are synced to the MongoDB database
+WHEN I open API GET routes in Insomnia Core for users and thoughts
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete users and thoughts in my database
+WHEN I test API POST and DELETE routes in Insomnia Core
+THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
+```
+   
+## Table of Contents
+- [Description](#description)
+- [User Story](#user-story)
+- [Acceptance Criteria](#acceptance-criteria)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Testing](#testing)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Installation
+💾   
+  
+`npm init`
+
+`npm install`
+  
+## Usage
+💻   
+  
+`npm start`
+
+With Nodemon:
+
+`npm run watch`
+
+## Testing
+✏️
+
+No current testing
+
+## Contributing
+
+
+## Questions
+✉️ Contact me with any questions: [email]() , [GitHub]()<br />
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,80 +1,75 @@
-const { Schema, model, Types }= require('mongoose');
+// Require Mongoos and Moment
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-// reaction schema
-const ReactionSchema = new Schema (
+// ReactionsSchema
+const ReactionsSchema = new Schema(
     {
-        //set custom ID
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-
-        },
-        reactionBody:  {
-           type: String,
-           required: true,
-           maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true
-
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a') 
-
-        }
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
     },
     {
-        toJSON: {
-            getters: true
-        }
+    toJSON: {
+        getters: true
+    } 
     }
 );
 
-// Thoughts Schema
-const ToughtsSchema = new Schema(
+// ThoughtsSchema
+const ThoughtsSchema = new Schema(
     {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            //moment
-            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-
-        },
-        username: {
-            type: String,
-            required: ture
-
-        },
-        reactions: [ReactionSchema]
-
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // Moment
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    // Use ReactionsSchema to validate data
+    reactions: [ReactionsSchema]
     },
     {
-        toJSON: {
-            virtuals: true,
-            getters: true
-
-        },
-        id: false
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
     }
-);
+)
 
-// get total count of reaction
-ThoughtsSchema.virtuals('reactionCount').get(function() {
+// get total count of reactions
+ThoughtsSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
-// create the thoughts model using the thoughts schema
+// create the Thoughts model using the Thoughts Schema
 const Thoughts = model('Thoughts', ThoughtsSchema);
 
+// Export Thoughts Module
 module.exports = Thoughts;
